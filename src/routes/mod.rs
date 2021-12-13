2fd3d855d760d74d27f452dc8e2bb011d4a3619f
Jacob <jacobbward0@gmail.com>
2021-12-13 15:32:50 +0000

    Deref DotPathBuf

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,7 @@
 pub mod core;
 pub mod s3;
 
+use std::ops::Deref;
 use std::path::PathBuf;
 
 use rocket::{request::FromSegments, http::uri::{fmt::Path, Segments, error::PathError}};
@@ -11,5 +12,12 @@ impl<'r> FromSegments<'r> for DotPathBuf {
     type Error = PathError;
     fn from_segments(segments: Segments<'r, Path>) -> Result<Self, Self::Error> {
         segments.to_path_buf(true).map(DotPathBuf)
+    }
+}
+
+impl Deref for DotPathBuf {
+    type Target = PathBuf;
+    fn deref(&self) -> &PathBuf {
+        &self.0
     }
 }
